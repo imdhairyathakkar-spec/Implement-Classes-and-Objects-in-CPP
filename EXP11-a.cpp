2fcab1460d549fa89ac66f3150f0f312dbdfa2d3
imdhairyathakkar-spec <imdhairyathakkar@gmail.com>
2025-10-01 17:55:43 +0530

    Create EXP11-a.cpp

---
+++ EXP11-a.cpp
@@ -0,0 +1,20 @@
+//Dhairya Thakkar
+//24070123037
+//A2
+#include<iostream>
+using namespace std;
+class cube{
+    public:
+    int height = 2;
+    int width = 3;
+    int length = 5;
+};
+int main (){
+    cube c1;
+    int vol=c1.height*c1.width*c1.length;
+    cout<< "volume  " << vol << endl;
+}
+
+/* OUTPUT:
+volume  30  
+*/
